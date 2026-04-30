b3d0ecb720dcda13ae0b7b42ae1c27677199be93
chokopiku61 <tim.23tim23.23@gmail.com>
2026-04-30 13:38:30 +0300

    Добавлена точка останова редукции

--- lib/lyambda_gem/reducer.rb
+++ lib/lyambda_gem/reducer.rb
@@ -8,6 +8,10 @@ module LyambdaGem
 
         step += 1
         puts "##{step} #{term}" if verbose
+        if (step > 1000)
+          puts "Редукция не завершилась за 1000 шагов, возможно бесконечный цикл. Прерывание."
+          break
+        end
       end
       term
     end
